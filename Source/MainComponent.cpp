3003ee8c21cbfd5f3c8fd9ff8106777c49064b4c
Sam Deakin <sam_d1995@hotmail.com>
2018-03-15 16:44:14 -0400

    Initial Commit
    A basic JUCE project.

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,77 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+#include <iostream>
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+
+    // specify the number of input and output channels that we want to open
+    setAudioChannels (0, 2);
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // This function will be called when the audio device is started, or when
+    // its settings (i.e. sample rate, block size, etc) are changed.
+
+    // Just save these settings for math later.
+    samplesPerBlock = samplesPerBlockExpected;
+    this->sampleRate = sampleRate;
+    inverseSampleRate = 1.0 / sampleRate;
+}
+
+void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+{
+    // Your audio-processing code goes here!
+
+    // For more details, see the help for AudioProcessor::getNextAudioBlock()
+
+    // Right now we are not producing any data, in which case we need to clear the buffer
+    // (to prevent the output of random noise)
+    bufferToFill.clearActiveBufferRegion();
+    time += bufferToFill.numSamples;
+    std::cout << bufferToFill.startSample << " " << bufferToFill.numSamples << " " << time << std::endl;
+}
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    // You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
---
+++ Source/MainComponent.h
@@ -0,0 +1,52 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent   : public AudioAppComponent
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent();
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    //==============================================================================
+    void paint (Graphics& g) override;
+    void resized() override;
+
+private:
+    int samplesPerBlock;
+    float sampleRate;
+
+    // 1 / sampleRate, for multiplication instead of division.
+    float inverseSampleRate;
+
+    /*
+     * Used to keep track of time.
+     * Essentially we increment this by samplesPerBlock every time we have to fill the buffer.
+     *
+     * We use an unsigned int here instead of a float specifically because it will loop back to 0 instead of
+     * slowly losing precision and then crashing, although at 44100 samples/sec that will happen after 27 hours.
+     * The time should be divided by the sample rate to get the time in seconds.
+     */
+    unsigned time;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
